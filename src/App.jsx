9ec500de9203f85faf638764cb33ca2d75d90c52
vinicius <viniciusalves@eaportal.org>
2025-05-16 12:19:59 -0300

    aprendendo_usar_variaveis_button

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import './App.css'
 import ButtonAlert from './components/ButtonAlert'
 
 function App() {
+  const [texto, setTexto] = useState("")
+  const handleTexto = (e) => {
+    
+    setTexto(e.target.value)
+  }
+
+
   const [ count, setCount ] = useState(1)
   const handleClik = () => {
     setCount(count*2)//o SetCount ele começa no 0 porque tem que esperar a pagina recarregar
@@ -15,8 +22,11 @@ function App() {
     <>
       
       <ButtonAlert mensagem="Mensagem do componente"> Saiba mais </ButtonAlert>
-    <div><button onClick={handleClik}>clique aqui</button>
-      {count}</div> 
+      <div>
+      <button onClick={handleClik }>clique aqui</button>
+      {textoq}
+      <input type="text" placeholder='digite aqui'  onChange={handleTexto}/>
+      </div> 
       
     </>
   )
@@ -26,3 +36,7 @@ export default App
 //usar o count no html na div do botao faz a conta aparecer do lado dele 
 
 //para usar variavies voce usa count e para iniciar e o setcount para pegar o valor
+
+// <input type="text" placeholder='digite aqui' /> e para inserir coisas usando o teclado
+
+// onchange e para quando se alterar algo na tag
